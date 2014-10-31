68e7142427f9ed8dbf803e642d32fdd5ae0917bc
JerryWang <gaediy@gmail.com>
2014-10-31 15:54:12 +0800

    modify

--- spider.js
+++ spider.js
@@ -4,13 +4,32 @@ var cheerio = require('cheerio');
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('yyets.db');
 var fs = require('fs');
-var done = false;
+var async = require('async');
+
+
+
 
 var sql = fs.readFileSync("./init.sql", "utf8");
 db.exec(sql);
 
 var config = fs.readFileSync("./config.json","utf8");
 config = JSON.parse(config);
+
+
+var url =  'http://www.yyets.com/resource/';
+var task = [];
+
+for(var i=0;i<config.count;i++){
+	task.push(url+parseInt(config.start+i));
+}
+async.eachLimit(task,5,spider,function(err){
+	console.log('all done');
+});
+
+
+
+
+// spider(url+i);
 
 function removeEle(ele){
 	ele.find('span').remove();
@@ -19,53 +38,76 @@ function removeEle(ele){
 }
 
 function parseLink(page,filmId){
-	var ul = page.find(".resod_list");
-	if(ul){
-		var lis = ul.find("li");
-		lis.each(function(){
-			var _this = cheerio(this);
-			var linkType = _this.find('.lks-0').text() || '';
-			if(linkType == '游戏')return true;
-			var link = _this.find(".lks-1>a").text() || '';
-			var filmSize = _this.find(".lks-2").text() || '';
-			var sql = "insert into films_link (filmId,link,linkType,filmSize) values (?,?,?,?)";
-			db.run(sql,[filmId,link,linkType,filmSize],function(){
-				sql = "insert into films_link_address (filmsLinkId,filmId,link,linkType) values (?,?,?,?)";
-				var download = _this.find(".download a");
-				var filmsLinkId = this.lastID;
-				download.each(function(){
-					var _this = cheerio(this);
-					linkType = _this.text();
-					if(linkType == '字幕')return true;
-					if(linkType == '电驴' || linkType == '免费盘'){
-						link = _this.attr('href');
-					}else if(linkType == '迅雷'){
-						link = _this.attr('thunderhref');
-					}else if(linkType == '旋风'){
-						link = _this.attr('qhref');
-					}else if(linkType == '小米'){
-						link = _this.attr('xmhref');
-					}
-					db.run(sql,[filmsLinkId,filmId,link,linkType]);
-				});
+	var lis = page.find(".resod_list li");
+	lis.each(function(){
+		var _this = cheerio(this);
+		var linkType = _this.find('.lks-0').text() || '';
+		if(linkType == '游戏'){return true;}
+		var link = _this.find(".lks-1>a").text() || '';
+		var filmSize = _this.find(".lks-2").text() || '';
+		var sql = "insert into films_link (filmId,link,linkType,filmSize) values (?,?,?,?)";
 
+		async.waterfall([function(cb){
+			db.run(sql,[filmId,link,linkType,filmSize],function(){
+				cb(null,this.lastID);
 			});
-		});
-	}
+		},function(filmsLinkId,cb){
+			sql = "insert into films_link_address (filmsLinkId,filmId,link,linkType) values (?,?,?,?)";
+			var download = _this.find(".download a");
+			download.each(function(){
+				var _this = cheerio(this);
+				linkType = _this.text();
+				if(linkType == '字幕')return true;
+				if(linkType == '电驴' || linkType == '免费盘'){
+					link = _this.attr('href');
+				}else if(linkType == '迅雷'){
+					link = _this.attr('thunderhref');
+				}else if(linkType == '旋风'){
+					link = _this.attr('qhref');
+				}else if(linkType == '小米'){
+					link = _this.attr('xmhref');
+				}
+				db.run(sql,[filmsLinkId,filmId,link,linkType]);
+			});
+		}]);
+		// db.run(sql,[filmId,link,linkType,filmSize],function(){
+		// 	sql = "insert into films_link_address (filmsLinkId,filmId,link,linkType) values (?,?,?,?)";
+		// 	var download = _this.find(".download a");
+		// 	var filmsLinkId = this.lastID;
+		// 	download.each(function(){
+		// 		var _this = cheerio(this);
+		// 		linkType = _this.text();
+		// 		if(linkType == '字幕')return true;
+		// 		if(linkType == '电驴' || linkType == '免费盘'){
+		// 			link = _this.attr('href');
+		// 		}else if(linkType == '迅雷'){
+		// 			link = _this.attr('thunderhref');
+		// 		}else if(linkType == '旋风'){
+		// 			link = _this.attr('qhref');
+		// 		}else if(linkType == '小米'){
+		// 			link = _this.attr('xmhref');
+		// 		}
+		// 		db.run(sql,[filmsLinkId,filmId,link,linkType]);
+		// 	});
+
+		// });
+	});
 }
 
 
-function spider(url){
+function spider(url,cb){
 	http.get(url,function(error,response,body){
 		var $;
 		try{
 			$ = cheerio.load(body);
 		}catch(e){
 			console.error('error at url:',url,'error msg is:',e);
+			cb();
 			return;
 		}
 		if($("#tipsMsg").text() == '资源不存在.页面将于3秒内自动跳转.'){
 			console.log('not exist at url:',url);
+			cb();
 			return;
 		}
 		var page = $(".AreaLL");
@@ -110,20 +152,23 @@ function spider(url){
 		_.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
 		var sql = "insert into films (title,channel,category,area,productCompany,language,firstTime,enName,enOtherName,officalWebsite,filmDesc,imgSrc,link)  values (?,?,?,?,?,?,?,?,?,?,?,?,? )";
 
-		db.run(sql,[title.trim(),channel.trim(),category.trim(),area.trim(),productCompany.trim(),language.trim(),firstTime.trim(),enName.trim(),enOtherName.trim(),officalWebsite.trim(),filmDesc.trim(),imgSrc,url],function(){
-				var id = this.lastID;
-				parseLink(page,this.lastID);
-				console.log(url,' done!');
+		async.waterfall([function(cb){
+			db.run(sql,
+				[title.trim(),channel.trim(),category.trim(),area.trim(),productCompany.trim(),language.trim(),firstTime.trim(),enName.trim(),enOtherName.trim(),officalWebsite.trim(),filmDesc.trim(),imgSrc,url],
+				function(){
+					cb(null,this.lastID);
 			});
+		},function(id,cb){
+			parseLink(page,id);
+			cb(null);
+		}],function(){
+			console.log('done ,url:',url);
+			cb();
+		});
+		// db.run(sql,[title.trim(),channel.trim(),category.trim(),area.trim(),productCompany.trim(),language.trim(),firstTime.trim(),enName.trim(),enOtherName.trim(),officalWebsite.trim(),filmDesc.trim(),imgSrc,url],function(){
+		// 		var id = this.lastID;
+		// 		parseLink(page,this.lastID,cb);
+		// 		console.log(url,' done!');
+		// 	});
 	});
 }
-
-// var url =  'http://www.yyets.com/resource/31560';
-var url =  'http://www.yyets.com/resource/';
-var max = config.start+config.max;
-for(var i = config.start ; i<max ; i++){
-	var currentUrl = url+i;
-	spider(currentUrl);
-}
-
-
